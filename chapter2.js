188a5955faccf743619b139de0596cc35abc3823
Mukesh Yaduvanshi <143539631+imcodey@users.noreply.github.com>
2023-09-29 14:01:31 +0000

    new file

---
+++ chapter2.js
@@ -0,0 +1,76 @@
+// let a = 2;
+// let b = 3;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a**b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a++) // 2
+// console.log(++a) //4
+// console.log(++a) //5
+// console.log(a--) //5
+// console.log(a++) //4
+// console.log(--a) //4
+// conasol.log(a) //4 
+
+
+// let a = 7;
+
+
+// console.log(a++)
+// console.log(a++)
+// console.log(a++)
+// console.log(a++)
+// console.log(--a)
+// console.log(a++)
+// console.log(--a)
+// console.log(a++)
+// console.log(--a)
+// console.log(a++)
+// console.log(a++)
+// console.log(a++)
+// console.log(a++)
+// console.log(--a)
+// console.log(--a)
+// console.log(--a)
+// console.log(a)
+
+// let x = 2;
+// let y = 1;
+// console.log(x+=y)
+// console.log(x-=y)
+// console.log(x*=y)
+// console.log(x/=y)
+// console.log(x%=y)
+// // console.log(x**=y)
+// console.log(x)
+
+// let a = 2;
+// let b = 2;
+
+// console.log(a<=b)
+
+
+// let a = prompt("Enter Your Budget")
+// a = Number.parseInt(a)
+// // console.log(typeof a)
+
+// if(a>=1000 && a<=2000){
+//     console.log("GTB Nagar With Riya");
+// }
+// else if(a>=2000 && a<=5000){
+//     console.log("Tilak Nagar with Dhanvi");
+// }
+// else if(a>=5000 && a<=10000){
+//     console.log("Kerala With Siddhi");
+// }
+// else{
+//     console.log("Leave your Miraz");   
+// }
+
+let a = prompt("Enter Your age")
+a = Number.parseInt(a) 
+console.log("You can",(a>=18)?"Drive":"Not Drive")
+
+
---
+++ chapter3.js
@@ -0,0 +1,43 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// for(i=1; i<=100; i++){
+//     console.log(i)
+// }
+
+const item ={
+    Mukesh:21,
+    Radha:17,
+    Ankush:17,
+    Jeenat:17,
+    Muskan:15,
+    Baby:16
+}
+
+// for(i=0; i<Object.keys(item).length; i++){
+//     console.log(Object.keys(item)[i], "Age is", item[Object.keys(item)[i]])
+// }
+// for(i in item){
+//     console.log(i, "Age is ", item[i])
+// }
+
+// let a = "Baby";
+
+// for(i of a){
+//     console.log(i)
+// }
+
+let i = 0;
+
+while(i<5){
+    console.log(i)
+}
+i++;
