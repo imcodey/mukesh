29655af1f7409181bc331131573e7e468a5f26a9
Mukesh Yaduvanshi <143539631+imcodey@users.noreply.github.com>
2023-10-16 13:44:41 +0000

    modified

--- chapter5.js
+++ chapter5.js
@@ -20,10 +20,81 @@
 // console.log(a)
 // console.log(a[10].length)
 
-let a = 'Mukesh';
-let b = 7
-let c = [1,2,3,4,5]
+// let a = 'Mukesh';
+// let b = 7
+// let c = [1,2,3,4,5]
 
-console.log(typeof a)
-console.log(typeof b)
-console.log(typeof c)
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+
+// let a1 = [1,2,3,4,5]
+// let a2 = [11,12,13,14,15]
+// let a3 = [21,22,23,24,25]
+// console.log(a)
+// console.log(a.toString())
+// console.log(a.join("-"))
+// a.pop()
+// a.push(11)
+// a.shift()
+// a.unshift(0)
+// delete a[5]
+// console.log(a.length)
+
+// console.log(a1.concat(a2,a3))
+
+// let a = [2,4,15,36,15,1,42,10,3,47,20]
+// a.sort((x,y)=>{
+//     return x-y
+// })
+// a.sort(function (x,y){
+//     return x-y
+// })
+// console.log(a)
+// let a = [1,2,3,4,5,6]
+// a.splice(5,2,23,24)
+// console.log(a)
+
+// let a = [1,2,3,4,5,6,7,8]
+// console.log(a.slice(2))
+
+// let a = [1,2,3,4,5,6,7,8]
+// let b = a.reverse()
+// console.log(b)
+
+// const a  = [1,2,3,4,5]
+// let b = a.forEach((value,index,array)=>{
+//         let b = value*value
+//         console.log(b)
+//     })
+// console.log(b)
+
+    // const a  = [1,2,3,4,5]
+    // let b = a.map((value,index,array)=>{
+    //     return value*value
+    // })
+    // console.log(b)
+
+// const a = [1,2,3,4,5,6,7,8,9,10]
+
+// const b = a.filter((x)=>{
+//     return x<5
+// })
+// console.log(b)
+
+// const num = [1,2,3,4,5,6,7,8]
+// let sum = num.reduce((x,y)=>x+y)
+// console.log(sum)
+
+// const a = ['a','b','c','d','e']
+
+// for(const element of a){
+//     console.log(a)
+// }
+
+const a = {a:1,b:2,c:3,d:4}
+
+for(const element in a){
+    console.log(`${element}:${object[element]}`)
+}
---
+++ chapter7.js
@@ -0,0 +1,11 @@
+// let a = document.body.firstElementChild
+// console.log(a)
+
+let a = document.body.childNodes
+console.log(a)
+
+// let a = document.body.childNodes[4]
+// console.log(a)
+
+// let a = document.body.childNodes[document.body.childNodes.length - 2]
+// console.log(a)
