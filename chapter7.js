a8dd528b2d24437e048473582d26058d630cf428
Mukesh Yaduvanshi <143539631+imcodey@users.noreply.github.com>
2023-10-17 14:32:10 +0000

    modified

--- chapter7.js
+++ chapter7.js
@@ -1,11 +1,32 @@
 // let a = document.body.firstElementChild
 // console.log(a)
 
-let a = document.body.childNodes
-console.log(a)
+// let a = document.body.childNodes
+// console.log(a)
 
 // let a = document.body.childNodes[4]
 // console.log(a)
 
 // let a = document.body.childNodes[document.body.childNodes.length - 2]
 // console.log(a)
+
+// console.log(document.body.previousSibling)
+// console.log(document.body.nextSibling)
+ 
+// let t = document.body.firstElementChild.firstElementChild.firstElementChild
+// let t = document.getElementsByTagName("table")
+// console.log(t)
+
+// let a = document.getElementById("id1")
+// a.style.color ="red"
+
+// let a = document.querySelectorAll(".ctitle")
+// a[0].style.color="red"
+// a[1].style.color="green"
+// a[2].style.color="blue"
+
+// let a = document.querySelector(".ctitle")
+// a.style.color="red"
+
+let a = document.getElementsByClassName("ctitle")
+a[0].style.color="red"
